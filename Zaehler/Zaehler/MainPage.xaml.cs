d5bb96294776f6c846d4ace1d06206936045774d
Murat <Murat@MURAT-LENOVOW10>
2022-02-26 16:29:05 +0100

    Add project files.

---
+++ Zaehler/Zaehler/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Zaehler
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private int _clickCount = 0;
+
+        private void ClickMeButtonOnClicked(object sender, EventArgs e)
+        {
+            _clickCount++;
+            CountLabel.Text = _clickCount == 1 ? "1 click" : $"{_clickCount} clicks";
+            ResetButton.IsEnabled = true;
+        }
+
+        private async void ResetButtonOnClicked(object sender, EventArgs e)
+        {
+            var accepted = await DisplayAlert("Sure", "Do you want reset the counter?", "yes", "no");
+            if (!accepted)
+                return;
+            _clickCount = 0;
+            CountLabel.Text = "0 clicks";
+            ResetButton.IsEnabled = false;
+
+        }
+    }
+}
